01f4273c10027fc57a906c84d38fbfe79608b9aa
ekosta <e.kostandini@forward.al>
2025-11-25 15:05:03 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
         <p>
-          Endow 2025
+          Endow 2025 V2
         </p>
       </div>
       <p className="read-the-docs">
